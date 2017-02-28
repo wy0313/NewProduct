3f1de118a233a40884c4fb041df6a952fb239c1e
wy0313 <849251004@qq.com>
2017-02-28 11:55:25 +0800

    新建

---
+++ NewProduct/www/config/router.config.js
@@ -0,0 +1,26 @@
+angular.module("myApp").config(function($stateProvider,$urlRouterProvider){
+	
+	$urlRouterProvider.when("", "/index");
+
+	$stateProvider
+	.state("index",{
+		url:"/index",
+		templateUrl:"./controllers/index/index.html",
+		controller:"indexCtrl"
+	})
+	.state("bank",{
+		url:"/bank",
+		templateUrl:"./controllers/bank/bank.html",
+		controller:"bankCtrl"
+	})
+	.state("know",{
+		url:"/know",
+		templateUrl:"./controllers/know/know.html",
+		controller:"knowCtrl"
+	})
+	.state("myself",{
+		url:"myself",
+		templateUrl:"./controllers/myself/myself.html",
+		controller:"myselfCtrl"
+	})
+})
