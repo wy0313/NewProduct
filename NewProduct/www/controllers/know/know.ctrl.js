cd36c92bfb477dce8e91cf5181ed5ae571284549
wutiaojian <2086628659@qq.com>
2017-03-02 12:19:01 +0800

    add know1

--- NewProduct/www/controllers/know/know.ctrl.js
+++ NewProduct/www/controllers/know/know.ctrl.js
@@ -1,3 +1,7 @@
-angular.module("myApp").controller("knowCtrl",function(){
-	
+angular.module("myApp").controller("knowCtrl",function($scope,$http){
+	$http.get("controllers/know/know.json")
+        .success(function(aa){
+        $scope.quan=aa.s;
+    })
+    
 })
