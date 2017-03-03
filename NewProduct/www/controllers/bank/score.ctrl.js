28df0586a2668ba7e518145799ccb2272d032f4f
wy0313 <849251004@qq.com>
2017-03-03 14:06:11 +0800

    hehe

--- NewProduct/www/controllers/bank/score.ctrl.js
+++ NewProduct/www/controllers/bank/score.ctrl.js
@@ -2,7 +2,7 @@ angular.module("myApp").controller("scoreCtrl",function($scope,$stateParams,$htt
 	$scope.list=[];
 	$http.get("http://119.10.11.121:8587/Api/Question/GetQuestionList?category_id="+$stateParams.id+"&user_id=1")
 	.then(function(data){
-		console.log(data.data.RetValue)
+		// console.log(data.data.RetValue)
 		var data=data.data.RetValue;	
 		$scope.list=data
 	})
--- NewProduct/www/server/userServer.js
+++ NewProduct/www/server/userServer.js
@@ -1,47 +1,84 @@
 angular.module('myApp').factory('userServer',function(httpServer,$ionicPopup,$state){
 	 
-	  return  {
-		  Register : function(options,successCallback,errorCallback){
-			   httpServer.post('Account/Register',{
-					LoginMobile : options.LoginMobile,
-					LoginCode : options.LoginCode, 
-					Pwd : options.Pwd
-			   },function( res ){
-					successCallback( res )
+	return  {
+		Register : function(options,successCallback,errorCallback){
+			httpServer.post('Account/Register',{
+				LoginMobile : options.LoginMobile,
+				LoginCode : options.LoginCode, 
+				Pwd : options.Pwd
+		    },function( res ){
+				successCallback( res )
+			},function( error ){
+				errorCallback( error )
+			})
+		},
+
+		Login:function(options,successCallback,errorCallback){
+			httpServer.post( 'Account/Login',{
+			   	LoginName : options.LoginName,
+			   	Pwd : options.Pwd
+			},function( res ){
+				if ( res.data.RetValue ){
+					window.localStorage['user'] = JSON.stringify( res.data.RetValue )    
+				}else{
+					var alertPopup = $ionicPopup.alert({
+						title: '提示',
+						template: res.data.description
+					});
+					alertPopup.then(function(res) {
+						console.log('Thank you for not eating my delicious ice cream cone');
+					});
+				}
+
+					successCallback( res.data )
+
 			   },function( error ){
+
 					errorCallback( error )
-			   })
-		  },
-		  Login:function(options,successCallback,errorCallback){
-			   httpServer.post( 'Account/Login',{
-					   LoginName : options.LoginName,
-					   Pwd : options.Pwd
-			   },function( res ){
-					   if ( res.data.RetValue ){
-							  window.localStorage['user'] = JSON.stringify( res.data.RetValue )    
-					   }else{
-							var alertPopup = $ionicPopup.alert({
-								   title: '提示',
-								   template: res.data.description
-								 });
-								 alertPopup.then(function(res) {
-								   console.log('Thank you for not eating my delicious ice cream cone');
-								 });
-							}
-							successCallback( res.data )
-							// console.log( res.data )
-			   },function( error ){
-					  errorCallback( error )
+
 			   } )  
 		  },
 
-		  getUserId : function(){
-			   if (window.localStorage['user']) {
-					return   JSON.parse( window.localStorage['user']).id
-			   }else{
+		getUserId : function(){
+			
+			if (window.localStorage['user']) {
+
+				return   JSON.parse( window.localStorage['user']).id;
+
+			}else{
 					  
-					$state.go('login') ;
-			   }
-		  }
-	  }
+				$state.go('login') ;
+			}
+		},
+
+		Parameters : function( options,successCallback,errorCallback ){
+
+            httpServer.post( "Account/RegisterSMSSend",{
+                mobile:options.mobile
+            },function( res ){
+                successCallback(res);
+            },function(error){
+                errorCallback( error );
+            })
+
+        },
+
+		Account : function( options,successCallback,errorCallback ){
+
+            httpServer.post( "Account/UpdateUserInfo",{
+                id : options.id,
+                nick_name : options.nick_name,
+                qq : options.qq,
+                email : options.email,
+                msn : options.msn
+
+	            },function( res ){
+	                successCallback(res);
+	            },function(error){
+	                errorCallback( error );
+	            })
+
+	        }
+
+	  	}
 })
